2b1d9703fe8c8fe3affbb78c0280f76a4e503723
Andrey Lezhen <lezhenandrey@gmail.com>
2025-03-12 13:40:10 +0500

    first commit

---
+++ Commits/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimpleWindowsFormsApp
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            Text = "Simple Windows Forms App";
+            Width = 300;
+            Height = 200;
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
