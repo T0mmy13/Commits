215411f12cb61bbf52d3d8dba74828cfff50db5e
Andrey Lezhen <lezhenandrey@gmail.com>
2025-03-12 13:41:37 +0500

    Second commit

--- Commits/Form1.cs
+++ Commits/Form1.cs
@@ -5,11 +5,26 @@ namespace SimpleWindowsFormsApp
 {
     public class MainForm : Form
     {
+        private Button showMessageButton;
+
         public MainForm()
         {
             Text = "Simple Windows Forms App";
             Width = 300;
             Height = 200;
+
+            showMessageButton = new Button();
+            showMessageButton.Text = "Show Message";
+            showMessageButton.Width = 120;
+            showMessageButton.Location = new System.Drawing.Point(90, 70);
+            showMessageButton.Click += ShowMessageButton_Click;
+
+            Controls.Add(showMessageButton);
+        }
+
+        private void ShowMessageButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, Windows Forms!");
         }
 
         [STAThread]
